7b691733f478c39c16bf0011b3037605bbcdb5df
Jose Pereira <jmanuelpereira2@gmail.com>
2020-05-07 21:53:20 -0400

    adding routes

--- routes/inventory.routes.js
+++ routes/inventory.routes.js
@@ -3,19 +3,26 @@ const router  = express.Router();
 const vehicleModel = require('../models/vehicle.model')
 
 //getting al inventory
-router.get('/all', (req, res, next) => {
+router.post('/all', (req, res, next) => {
   vehicleModel.find()
-              .sort({ date: -1 })
-              .then(allData => res.json(allData))
+
+              .then(allData => res.status(200).json(allData))
               .catch(err => next(err))
 
-              res.status(200).json(allData)
+             
 
 });
 
 //agregar carros
 
 router.post('/add', (req, res, next) => {
+
+  res.json(console.log("Received"))
+  
+});
+
+router.post('/add-vehicle', (req, res, next) => {
+  const { model, year, make, plate } = req.body
   vehicleModel.create(req.body)
   .then(vehicleInfo => res.status(200).json({ car: vehicleInfo, message: "Received"}))
   .catch(err => next(err));
@@ -25,11 +32,11 @@ router.post('/add', (req, res, next) => {
  
 
 //getting one vehicle
-router.get('/search:carId', (req, res, next) => {
+router.post('/search:carId', (req, res, next) => {
 
 
 
-  vehicleModel.findOne({id_: req.params.carId})
+  vehicleModel.findOne({_id:req.params.carId})
               
               .sort({ date: -1 })
               .then(allData => {
